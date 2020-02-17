c323f40d713cf3c45ab0c79fb738495fb80c5829
Erastus Murungi <erastuswagwan@gmail.com>
2020-02-16 20:10:29 -0500

    Update conway.py

--- conway.py
+++ conway.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-def conway(rows, cols, num_zeroes, vid):
+def conway(rows, cols, num_zeroes, with_mod=False, vid=False):
     on, off = 1, 0
     Writer = animation.writers['ffmpeg']
     writer = Writer(fps=15, metadata=dict(artist='Erastus'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
@@ -21,7 +21,7 @@ def conway(rows, cols, num_zeroes, vid):
             anim.save('conway.mp4', writer=writer)
         plt.show()
 
-    def _neighbors(r, c, state):
+    def _neighbors_with_mod(r, c, state):
         _c = (c - 1) % cols
         _r = (r - 1) % rows
         c_ = (c + 1) % cols
@@ -30,9 +30,24 @@ def conway(rows, cols, num_zeroes, vid):
                      state[r_][_c], state[r_][c], state[r_][c_]])
         return total
 
+    def _neighbors_without_mod(r, c, state):
+        total, _c, _r, c_, r_ = 0, (c - 1), (r - 1), (c + 1), (r + 1)
+
+        total += state[r][c_] if c_ < cols else 0
+        total += state[r][_c] if _c >= 0 else 0
+        total += state[r_][c] if r_ < rows else 0
+        total += state[r_][_c] if r_ < rows and _c >= 0 else 0
+        total += state[r_][c_] if r_ < rows and c_ < cols else 0
+        total += state[_r][_c] if _r >= 0 and _c >= 0 else 0
+        total += state[_r][c] if _r >= 0 else 0
+        total += state[_r][c_] if _r >= 0 and c_ < cols else 0
+        return total
+
+    get_neighbors = _neighbors_with_mod if with_mod else _neighbors_without_mod
+
     def _update_state(_, img, state):
         for i, j in iterations:
-            outdeg = _neighbors(i, j, state)
+            outdeg = get_neighbors(i, j, state)
             if state[i, j] == on:
                 if outdeg < 2 or outdeg > 3:
                     state[i][j] = off
@@ -47,4 +62,4 @@ def conway(rows, cols, num_zeroes, vid):
 
 
 if __name__ == '__main__':
-    conway(100, 100, 20, False)
+    conway(100, 100, 16)
